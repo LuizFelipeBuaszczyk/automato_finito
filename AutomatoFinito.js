d89176cbbc88619535c40f797f469c2dd219158d
LuizFelipeBuaszczyk <luizfelipebuaszczyk@hotmail.com>
2025-10-22 13:05:47 -0300

    fix: Tratatando o automato finito como um vetor de dicionarios
    Dessa forma cada estado q consegue visualizar quais letras e para qual estado ele ira de acordo com o dicionário

--- AutomatoFinito.js
+++ AutomatoFinito.js
@@ -1,9 +1,5 @@
 let stateQ = [] // Estados q0, q1, q2...
 
-// TOFIX: A lógica ainda está meio confusa e bagunçada
-// Quando recebe uma nova letra ele deve criar uma nova regra
-// TESTE e TESTES   || A regra 4 deve ser (E, -1) e (E, 5) -- Cria uma nova regra
-// TESTE e TESTANDO || A regra 3 deve ser (T,4) e (T,5), onde a regra 5 tem (A, 6)
 
 // Recebe uma string e adiciona ao autômato existente
 export async function generateFiniteAutomata(word) {
@@ -12,50 +8,47 @@ export async function generateFiniteAutomata(word) {
 
     for (let i = 0; i < word.length; i++) {
         let char = word[i];
-
-        let nextState = i === word.length - 1 ? -1 : posStateQ + 1;
+        let nextChar = word.length - 1 == i ? "EPSILON" : word[i+1];
+        console.log(char, posStateQ)
 
         // Se o autômato ainda está vazio, cria o primeiro estado
-        if (stateQ.length === 0) {
-            stateQ.push([[char, nextState]]);
-            posStateQ = nextState === -1 ? 0 : nextState;
+        if (stateQ.length == 0) {
+            let init_rule = {
+                [char] : 1
+            } 
+            stateQ.push(init_rule);
+            posStateQ = 1;
             continue;
         }
 
-        // Pega as regras do estado atual
-        const rules = stateQ[posStateQ] || [];
-        let found = false;
+        let rules = stateQ[posStateQ]
 
-        for (let j = 0; j < rules.length; j++) {
-            if (rules[j][0] === char) {
-                found = true;
+        if (!rules){
+            posStateQ = stateQ.length + 1
+            let not_rule = {
+                [char]: posStateQ
+            }
+            stateQ.push(not_rule);
+        }
+        else{
 
-                // Se a transição já está correta -> apenas segue para o próximo estado
-                if (rules[j][1] === nextState) {
-                    posStateQ = nextState === -1 ? 0 : nextState;
-                } else {
-                    rules.push([char, nextState]);
-                    posStateQ = nextState === -1 ? 0 : nextState;
-                }
-                break;
+            if(!(char in rules)){
+                rules[char] = stateQ.length + 1;
+                stateQ[posStateQ] = rules;
+                posStateQ = stateQ.length + 1
+            }
+            else{
+                posStateQ++;
             }
         }
-
-        // Se não encontrou nenhuma transição, cria nova
-        if (!found) {
-            stateQ[posStateQ] = rules; 
-            rules.push([char, nextState]);
-
-            // Se o próximo estado não é final, cria o array do estado
-            if (nextState !== -1 && !stateQ[nextState]) {
-                stateQ[nextState] = [];
-            }
-
-            posStateQ = nextState === -1 ? 0 : nextState;
+        if (nextChar == "EPSILON"){
+            stateQ.push({[nextChar]: -1})
         }
+
     }
-
-    console.log(stateQ);
+    
+    console.log(stateQ)
+        
 }
 
 export async function resetStateQ() {
